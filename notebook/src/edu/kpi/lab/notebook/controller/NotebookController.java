6d56b62d136dbcc65a7df3c3e107b0b09c7e576a
Eugene <pashkovskiy.eugen@gmail.com>
2022-05-31 12:22:36 +0300

    Deleted unnecessary files

--- notebook/src/edu/kpi/lab/notebook/controller/NotebookController.java
+++ notebook/src/edu/kpi/lab/notebook/controller/NotebookController.java
@@ -5,14 +5,11 @@ import edu.kpi.lab.notebook.model.NotebookItem;
 import edu.kpi.lab.notebook.view.InputView;
 import edu.kpi.lab.notebook.view.NotebookView;
 
-import java.util.ArrayList;
-
-public class NotebookController implements INotebookController {
-	private final Notebook notebook;
+public class NotebookController {
 
 	public NotebookController() {
-		this.notebook = new Notebook();
-		this.notebook.addItem(new NotebookItem("Pashkovsky", "Eugene", "Serhiyovich", "??", "+3333", "-"));
+		Notebook notebook = new Notebook();
+		notebook.addItem(new NotebookItem("Pashkovsky", "Eugene", "Serhiyovich", "??", "+3333", "-"));
 		boolean finished = false;
 		while (!finished) {
 			NotebookView view = new NotebookView();
@@ -22,16 +19,16 @@ public class NotebookController implements INotebookController {
 			int chosenOption = input.getMenuOptionInput();
 			switch (chosenOption) {
 				case 1:
-					view.printResult(this.notebook.getListOfItems());
+					view.printResult(notebook.getListOfItems());
 					input.waitUntilKeyIsPressed();
 					break;
 				case 2:
 					String letter = input.getInputLetter();
-					view.printResult(this.notebook.findByFirstLetterOfSurname(letter));
+					view.printResult(notebook.findByFirstLetterOfSurname(letter));
 					input.waitUntilKeyIsPressed();
 					break;
 				case 3:
-					view.printResult(this.notebook.filterByTelephoneExistence());
+					view.printResult(notebook.filterByTelephoneExistence());
 					input.waitUntilKeyIsPressed();
 					break;
 				case 4:
@@ -41,20 +38,5 @@ public class NotebookController implements INotebookController {
 					break;
 			}
 		}
-	}
-
-	@Override
-	public Notebook getNotebookInstance() {
-		return this.notebook;
-	}
-
-	@Override
-	public ArrayList<NotebookItem> findByFirstLetterOfSurname(String letter) {
-		return this.notebook.findByFirstLetterOfSurname(letter);
-	}
-
-	@Override
-	public ArrayList<NotebookItem> filterByTelephoneExistence() {
-		return this.notebook.filterByTelephoneExistence();
 	}
 }
